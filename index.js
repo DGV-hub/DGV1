0428fa6edf866cdfc774e0400ea7a0776f752951
Shane <shane@lefaagency.com>
2025-12-31 00:03:34 +0200

    update 6

--- index.js
+++ index.js
@@ -9,8 +9,4 @@ res.send("Patreon → Telegram Auth Server is running");
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log("Server running on port", PORT);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 01e811f13da3c48b4a97cfc11548cdddaa9200e0
