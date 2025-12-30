8d279d3911a704b1137c8c9dd5d5ceaa8b1ad8ef
Shane <shane@lefaagency.com>
2025-12-31 00:14:50 +0200

    update 7

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+"name": "patreon-telegram-auth",
+"version": "1.0.0",
+"main": "index.js",
+"type": "module",
+"scripts": {
+"start": "node index.js"
+},
+"dependencies": {
+"express": "^4.19.2",
+"axios": "^1.6.7",
+"dotenv": "^16.4.5"
+}
+}
